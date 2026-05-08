9f9ff4bc9d52bd7728ad82dd6cc725a80f916bb3
Samia Azme <samiaazme213@gmail.com>
2026-05-08 17:16:41 +0600

    Changes the language in JS

--- CodeChef/203_Cricket World Cup Qualifier.js
+++ CodeChef/203_Cricket World Cup Qualifier.js
@@ -1,18 +1,27 @@
 // Cricket World Cup Qualifier
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-readline.question('', (input) => {
-  const x = parseInt(input);
-  // Write your code here
-    if (x >= 12){
+function solve(take)
+{
+    let x = Number(take);
+   if (x >= 12){
         console.log("Yes");
     }
     else {
         console.log("No");
     }
-  readline.close();
+}
+
+
+// Boilerplate for JS
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+
+process.stdin.on('data', function (chunk) {
+    inputString += chunk;
+});
+
+process.stdin.on('end', function () {
+    solve(inputString);
 });
--- CodeChef/213_Lucky Seven.js
+++ CodeChef/213_Lucky Seven.js
@@ -1,12 +1,24 @@
 // Lucky Seven
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
+function solve(take)
+{
+    let str = take.split("");
+    let luckyLetter = str[6];
+    console.log(luckyLetter);
+    
+    
+}
+
+// Boilerplate for JS
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+
+process.stdin.on('data', function (chunk) {
+    inputString += chunk;
 });
 
-readline.question('', (input) => {
-    var S = String(input);
-    console.log(S[6]);
-  readline.close();
+process.stdin.on('end', function () {
+    solve(inputString);
 });
