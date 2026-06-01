8b3321c5b2a0d9c92ae55c18fd66453deb01f62a
Samia Azme <samiaazme213@gmail.com>
2026-06-01 23:58:15 +0600

    need some fixing

---
+++ CodeChef/348_SleepDeprivation.js
@@ -0,0 +1,36 @@
+function solve(input)
+{
+    let sleep = Number(input)
+    let T = Number(sleep[0])
+    
+    for(let i = 1; i <= T; i++)
+    {
+        let X = Number(sleep[i])
+        if( X < 7 )
+        {
+            console.log("YES")
+        }
+        else
+        {
+            console.log("NO")
+        }
+    }
+        
+    
+}
+
+// Boilerplate for JS
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = "";
+
+process.stdin.on('data', function (chunk) {
+    inputString += chunk;
+});
+
+process.stdin.on('end', function () {
+    solve(inputString);
+});
+
+//thik krte hbe
