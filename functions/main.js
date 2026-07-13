77d234f99d0983bc5c8e4628b46d1be092420f91
Samia Azme <samiaazme213@gmail.com>
2026-07-14 00:35:15 +0600

    learning plot

---
+++ functions/main.js
@@ -0,0 +1,25 @@
+// // let arr = [3,4,5,6,7]
+// let arr = [];
+// for(let i = 3; i <=74; i++)
+// {
+//     arr.push(i)
+// }
+// // arr.push(3);
+// // arr.push(4);
+// console.log(arr);
+
+// let arr = [];
+// for(let i = 569; i <=10999; i++)
+// {
+//     arr.push(i)
+// }
+// console.log(arr);
+
+import { pocket } from "./pockets.js"
+
+let arr2 = pocket(25, 35)
+// if(arr2 % 2 == 0)
+// {
+
+// }
+console.log(arr2)
---
+++ functions/pockets.js
@@ -0,0 +1,8 @@
+ export function pocket(a, b) {
+  let arr = [];
+  for (let i = a; i <= b; i++) {
+    arr.push(i);
+  }
+  return arr;
+  console.log(arr);
+}
